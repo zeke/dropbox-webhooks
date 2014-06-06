fc104c0a386cbc94e1634edda3abe9bd93a21ad1
zeke <zeke@sikelianos.com>
2014-06-05 19:41:49 -0700

    express

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 "use strict"
 
 require("dotenv").load()
+var app = require("express")()
+
+app.get('/', function(req, res) {
+  res.send(req.query.challenge)
+})
+
+var server = app.listen((process.env.PORT || 5000), function() {
+  console.log('Listening on port %d', server.address().port)
+})
