911dc55947df4b56de2c00af9242c54009cd617b
zeke <zeke@sikelianos.com>
2014-06-05 19:49:41 -0700

    add POST endpoint

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ app.get('/', function(req, res) {
   res.send(req.query.challenge)
 })
 
+app.post('/', function(req, res) {
+  console.log(req.body)
+  res.json(req.body)
+})
+
 var server = app.listen((process.env.PORT || 5000), function() {
   console.log('Listening on port %d', server.address().port)
 })
